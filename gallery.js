63c0cd04ddf808bb03d99eff6d2f19cc96f1e0d5
Yakiv Shypelyk <schypelyk128@ukr.net>
2020-10-13 16:44:06 +0300

    change gallery.js

--- gallery.js
+++ gallery.js
@@ -3,37 +3,35 @@ import galleryImagesEl from "./gallery-items.js"
 const galleryEl = document.querySelector('.js-gallery');
 const modalLightboxEl = document.querySelector('.js-lightbox');
 const overlayEl = document.querySelector('.lightbox__overlay');
-const modalImageEl = document.querySelector('lightbox__image');
-const buttonEl = document.querySelector('lightbox__button');
+const modalImageEl = document.querySelector('.lightbox__image');
+const buttonEl = document.querySelector('.lightbox__button');
 
 const galleryListEl = createGalleryList(galleryImagesEl);
 
 galleryEl.insertAdjacentHTML("beforeend", galleryListEl);
 
 galleryEl.addEventListener('click', onOpenModal);
-// buttonEl.addEventListener('click', onCloseModal);
-// overlayEl.addEventListener('click', onCloseModalOnOverlay);
+buttonEl.addEventListener('click', onCloseModal);
+overlayEl.addEventListener('click', onCloseModalOnOverlay);
+document.addEventListener('keydown', onCloseModalEsc);
 
   function createGalleryList(img) {
     return  img.map(({original, description, preview}) => {
       return `<li class="gallery__item">
-    <a
-      class="gallery__link"
-      href="${original}"
-    >
-      <img
-        class="gallery__image"
-        src="${preview}"
-        data-source="${original}"
-        alt="${description}"
-      />
-    </a>
-  </li>`}).join("");
+      <a
+        class="gallery__link"
+        href="${original}"
+      >
+        <img
+          class="gallery__image"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
+        />
+      </a>
+    </li>`}).join("");
   }  
  
-
-// document.addEventListener('keydown', onCloseModalEsc);
-
 function onOpenModal(evt) {
     evt.preventDefault();
     const isGalleryImage = evt.target.classList.contains('gallery__image');
@@ -47,22 +45,21 @@ function onOpenModal(evt) {
     modalImageEl.alt = evt.target.alt;
 }
 
-// function onCloseModal(evt) {
-//     document.removeEventListener('keydown', onCloseModalEsc);
-//     modalLightboxEl.classList.remove("is-open");
-//     modalImageEl.src = "";
-// }
+function onCloseModal() {
+    modalLightboxEl.classList.remove("is-open");
+    modalImageEl.src = "";
+}
 
-// function onCloseModalOnOverlay(evt) {
-//     if (evt.currentTarget === evt.target) {
-//         onCloseModal();
-//     }
-// }
-// function onCloseModalEsc (evt) {
-//     if (evt.code === "Escape") {
-//         onCloseModal();
-//     }
-// }
+function onCloseModalOnOverlay(evt) {
+    if (evt.currentTarget === evt.target) {
+        onCloseModal();
+    }
+}
+function onCloseModalEsc (evt) {
+    if (evt.code === "Escape") {
+        onCloseModal();
+    }
+}
 
 
 
